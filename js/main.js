2ffeb03b53cf70060c3101255fb5ceb5a86abdd3
sagehartly <sage.hartly@gmail.com>
2016-02-26 16:46:50 -0500

    Added first loop

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var newLetter = prompt('Enter a Letter');
+
+var counter;
+var counter2;
+
+for (counter = 0; counter < 10; counter++) {
+  for (counter2 = 0; counter2 ; counter2++)
+  document.write(newLetter);
+
+  document.write('<br>')
+
+}
