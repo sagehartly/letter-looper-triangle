e95b9ddedbe015ade7631531a108cd82d717b061
sagehartly <sage.hartly@gmail.com>
2016-02-26 16:51:10 -0500

    Fixed missing css

--- js/main.js
+++ js/main.js
@@ -4,9 +4,8 @@ var counter;
 var counter2;
 
 for (counter = 0; counter < 10; counter++) {
-  for (counter2 = 0; counter2 ; counter2++)
-  document.write(newLetter);
-
-  document.write('<br>')
-
+  for (counter2 = 0; counter2 <= counter; counter2++) {
+    document.write(newLetter);
+  }
+  document.write('<br>');
 }
